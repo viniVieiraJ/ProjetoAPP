90a24616154921b41c5997c880103263b84ddcd2
DamnVini <vinicius_v_jesus@estudante.sc.senai.br>
2019-07-08 20:53:38 -0300

    ADIÇÃO

--- principal.js
+++ principal.js
@@ -42,3 +42,51 @@ function imprimirArray(id, array) {
         span.innerHTML += array[i].name + ' (PEGI: ' + array[i].PEGI + ', genero: ' + array[i].genre + ')<br>';
     }
 }
+
+
+var listElement = document.querySelector("#app ul")
+var inputElement = document.querySelector("#app input")
+var buttonElement = document.querySelector("#app button")
+
+var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
+
+
+function renderTodos() {
+    listElement.innerHTML = "";
+    for (todo of todos) {
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement("a");
+
+        linkElement.setAttribute("href", "#");
+
+        var pos = todos.indexOf(todo);
+
+        linkElement.setAttribute("onclick", "DeleteTodo(" + pos + ")")
+
+        var linkText = document.createTextNode(" Excluir");
+
+        linkElement.appendChild(linkText);
+
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = "";
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+
